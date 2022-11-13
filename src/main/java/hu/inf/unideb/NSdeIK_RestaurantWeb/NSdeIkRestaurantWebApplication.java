40197a610e36d6ef1a16d8daa5b775a3ee41d505
NSdeIK <71137188+NSdeIK@users.noreply.github.com>
2022-11-13 14:32:01 +0100

    Alap projekt előkészítése

---
+++ src/main/java/hu/inf/unideb/NSdeIK_RestaurantWeb/NSdeIkRestaurantWebApplication.java
@@ -0,0 +1,13 @@
+package hu.inf.unideb.NSdeIK_RestaurantWeb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NSdeIkRestaurantWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NSdeIkRestaurantWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hu/inf/unideb/NSdeIK_RestaurantWeb/NSdeIkRestaurantWebApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.inf.unideb.NSdeIK_RestaurantWeb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NSdeIkRestaurantWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
