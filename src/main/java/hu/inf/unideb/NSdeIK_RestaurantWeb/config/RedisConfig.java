e0ace41ea30762b14404766092b793a3d8cf4e8c
NSdeIK <71137188+NSdeIK@users.noreply.github.com>
2022-11-15 22:15:15 +0100

    Redis DB konfiguráció hozzáadása #2

---
+++ src/main/java/hu/inf/unideb/NSdeIK_RestaurantWeb/config/RedisConfig.java
@@ -0,0 +1,37 @@
+package hu.inf.unideb.NSdeIK_RestaurantWeb.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+
+@Configuration
+public class RedisConfig
+{
+    @Value("${redis.host}")
+    String host;
+
+    @Value("${redis.port}")
+    Integer port;
+
+    @Value("${redis.password}")
+    String password;
+
+    @Bean
+    public JedisConnectionFactory redisConnectionFactory(){
+        RedisStandaloneConfiguration config = new RedisStandaloneConfiguration(host,port);
+        config.setPassword(password);
+        return new JedisConnectionFactory(config);
+    }
+
+    @Bean
+    public RedisTemplate<String,Object> redisTemplate(){
+        RedisTemplate<String,Object> template = new RedisTemplate<>();
+        template.setConnectionFactory(redisConnectionFactory());
+        return template;
+    }
+
+}
+
